0ffcf41d3b4ba6b3e8a0b348d574a4c107acbf3e
V Sreekanth <sreeix@gmail.com>
2015-09-04 18:24:54 +0530

    Adding leader election code.

---
+++ test/recipies/leaderElectionTest.js
@@ -0,0 +1,30 @@
+"use strict";
+var zookeeper = require("node-zookeeper-client");
+var leadership = require("../../lib/recipies/leaderElection");
+
+describe("leaderelection", function() {
+    var client;
+    var zkLib;
+
+    beforeEach(function(done) {
+        client = zookeeper.createClient('localhost:2181');
+        client.connect();
+        client.once('connected', function () {
+            console.log("Connected to zookeeper");
+            zkLib = require('../../lib/zkLib')(client);
+            done();
+        });
+    });
+    afterEach(function () {
+        console.log("Closing Zk Connection");
+        client.close();
+    });
+
+    it("takes leadership when only one node", function(done) {
+        var le = leadership(client);
+        le.register("/bjn/tests/leadership", function onLeader(event) {
+            console.log("Became Leader");
+            done();
+        });
+    });
+});
