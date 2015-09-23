dd1a6b747bf00c90ec1eb71a5968e1fa02e2a818
V Sreekanth <sreeix@gmail.com>
2015-09-23 23:42:09 +0530

    Adding the allsitesabort optimization

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     lock: require('recipies/lock'),
     twoPhaseCommit: require('recipies/2pc'),
+    leaderElection: require('recipies/leaderElection'),
     persistentEphemeralNode: require('recipies/persistentEphemeralNode')
 };
--- test/recipies/2pc_test.js
+++ test/recipies/2pc_test.js
@@ -55,94 +55,97 @@ describe('2 pc', function  () {
         console.log("Closing Zk Connection");
         client.close();
     });
+    describe(" strict 2pc", function() {
+        describe("simple one site tests", function  () {
+            it('should successfully execute', function  (done) {
+                coordinator.execute('update', {sites: [comittingSite()], quorum: true}, done);
+            });
+            it('should successfully execute with quorum=1', function  (done) {
+                coordinator.execute('update', {sites: [comittingSite()], quorum: 1}, done);
+            });
+            it('should abort on default timeout(5000 ms)', function  (done) {
+                coordinator.execute('update', {sites: [comittingSite('slow-comitting', 8000)], quorum: true}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
+            });
+            it('should abort', function  (done) {
+                coordinator.execute('update', {sites: [abortingSite()], quorum: true}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
+            });
+        });
 
-    describe("simple one site tests", function  () {
-        it('should successfully execute', function  (done) {
-            coordinator.execute('update', {sites: [comittingSite()], quorum: true}, done);
-        });
-        it('should successfully execute with quorum=1', function  (done) {
-            coordinator.execute('update', {sites: [comittingSite()], quorum: 1}, done);
-        });
-        it('should abort on default timeout(5000 ms)', function  (done) {
-            coordinator.execute('update', {sites: [comittingSite('slow-comitting', 8000)], quorum: true}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+        describe("For 2 sites", function  () {
+            it('should commit with quorum when 2 sites commit', function  (done) {
+                coordinator.execute('update', {sites: [comittingSite(), comittingSite()], quorum: true}, done);
+            });
+
+            it('should abort with quorum when 1 site aborts', function  (done) {
+                coordinator.execute('update', {sites: [abortingSite(), comittingSite()], quorum: true}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
+            });
+            it('should abort on default timeout(5000 ms)', function  (done) {
+                coordinator.execute('update', {sites: [comittingSite('slow-comitting', 8000), comittingSite('slow-comitting', 3000)], quorum: true}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
+            });
+
+            it('should commit with quorum = 1 when first site says abort', function  (done) {
+                coordinator.execute('update', {sites: [abortingSite('slow-site-'+random(10000), 10), comittingSite('fast-site-'+random(10000), 1000)], quorum: 1}, done);
+            });
+
+            it('should commit with quorum = 1 when first site says commit and second abort', function (done) {
+                coordinator.execute('update', {sites: [abortingSite('slow-site-'+random(10000), 300), comittingSite('fast-site-'+random(10000), 10)], quorum: 1}, done);
+            });
+
+            it('should abort with quorum, when 1 site aborts even if first is success', function  (done) {
+                coordinator.execute('update', {sites: [abortingSite('slow-site', 300), comittingSite('fast-site', 10)], quorum: true}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
+            });
+
+            it('should abort with quorum of when 1 site aborts and 1 commits', function  (done) {
+                coordinator.execute('update', {sites: [abortingSite(), comittingSite()], quorum: true}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
+            });
+
+            it('should abort with quorum of when 1 site aborts and 1 commits', function  (done) {
+                coordinator.execute('update', {sites: [abortingSite(), comittingSite()], quorum: false}, function  (err, result) {
+                    if(err){
+                        // we were expecting this. should be abort.
+                        return done();
+                    }
+                    return done("Unexpected. We were expecting an abort. As writing to tlog failed");
+                });
             });
         });
-        it('should abort', function  (done) {
-            coordinator.execute('update', {sites: [abortingSite()], quorum: true}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
-            });
-        });
+
     });
 
-    describe("For 2 sites", function  () {
-        it('should commit with quorum when 2 sites commit', function  (done) {
-            coordinator.execute('update', {sites: [comittingSite(), comittingSite()], quorum: true}, done);
-        });
-
-        it('should abort with quorum when 1 site aborts', function  (done) {
-            coordinator.execute('update', {sites: [abortingSite(), comittingSite()], quorum: true}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
-            });
-        });
-        it('should abort on default timeout(5000 ms)', function  (done) {
-            coordinator.execute('update', {sites: [comittingSite('slow-comitting', 8000), comittingSite('slow-comitting', 3000)], quorum: true}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
-            });
-        });
-
-        it('should commit with quorum = 1 when first site says abort', function  (done) {
-            coordinator.execute('update', {sites: [abortingSite('slow-site-'+random(10000), 10), comittingSite('fast-site-'+random(10000), 1000)], quorum: 1}, done);
-        });
-
-        it('should commit with quorum = 1 when first site says commit and second abort', function (done) {
-            coordinator.execute('update', {sites: [abortingSite('slow-site-'+random(10000), 300), comittingSite('fast-site-'+random(10000), 10)], quorum: 1}, done);
-        });
-
-        it('should abort with quorum, when 1 site aborts even if first is success', function  (done) {
-            coordinator.execute('update', {sites: [abortingSite('slow-site', 300), comittingSite('fast-site', 10)], quorum: true}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
-            });
-        });
-
-        it('should abort with quorum of when 1 site aborts and 1 commits', function  (done) {
-            coordinator.execute('update', {sites: [abortingSite(), comittingSite()], quorum: true}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
-            });
-        });
-
-        it('should abort with quorum of when 1 site aborts and 1 commits', function  (done) {
-            coordinator.execute('update', {sites: [abortingSite(), comittingSite()], quorum: false}, function  (err, result) {
-                if(err){
-                    // we were expecting this. should be abort.
-                    return done();
-                }
-                return done("Unexpected. We were expecting an abort. As writing to tlog failed");
-            });
-        });
-    });
 });
